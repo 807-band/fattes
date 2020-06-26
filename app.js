7669fc5c55bcda66886cc712cdf9c87ef7bfb03f
Ethan Zimbelman <itszimbo@gmail.com>
2020-06-26 16:37:24 -0500

    the beginning

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const connectToDB = require('./config/db.js');
+
+router.get('/', (req, res) => {
+   res.send("Hello World!");
+});
+
+var port = process.env.PORT || 3000;
+
+connectToDB();
+app.use('/', router);
+app.listen(port, () => console.log(`Server running on port ${port}));
---
+++ config/db.js
@@ -0,0 +1,18 @@
+const dynamoose = require('dynamoose');
+
+const db = new dynamoose.aws.sdk.DynamoDB({
+   "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+   "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+   "region": process.env.AWS_REGION
+});
+
+const connectToDB = async () => {
+   try {
+      dynamoose.aws.ddb.set(db);
+      console.log("Connected to DynamoDB");
+   } catch (err) {
+      console.log(err);
+   }
+};
+
+module.exports = connectToDB;
